ad6cf8dab10a7aaced6898b98569f99d7d1397b9
Joan <jlloppalao@gmail.com>
2020-01-05 13:57:26 +0100

    game improvements + tanh

--- game_improved.py
+++ game_improved.py
@@ -3,8 +3,6 @@ from __future__ import print_function
 import multiprocessing
 import os
 
-import visualize
-
 import neat
 import numpy as np
 import gym
@@ -15,22 +13,29 @@ import custom_report
 
 env = gym.make('CarRacing-v0')
 
-NUM_GENERATIONS = 100
+NUM_GENERATIONS = 1000
+
+def map_image(pixel):
+    if pixel[1] < 200:
+        return 1
+    return 0
+
 
 def preprocess(frame):
     """
     This function receives a frame of the game and performs mean pooling in kernels od 2x2x3, to reduce dimensionality. Resulting 
     """
+    # Simplify the map by setting the road to 1 and everything else to 0
+    frame = np.array([[map_image(pixel) for pixel in row] for row in frame])
     frame = frame[:83, :]
-    frame = block_reduce(frame, (2, 2, 3), np.mean)
+    frame = block_reduce(frame, (5, 5), np.mean)
     frame = frame.flatten()
-#     print(frame.shape)
     return frame
 
 
 def eval_network(net, frame):
-    frame = preprocess(frame)
-    assert (frame.shape == (2016,))
+#     print(frame.shape)
+    assert (frame.shape == (340,))
     result = net.activate(frame)
     assert (len(result) == 3)
     return result
@@ -45,13 +50,22 @@ def eval_genome(genome, config):
     net = neat.nn.FeedForwardNetwork.create(genome, config)
     total_reward = 0.0
     frame = env.reset()
+    frame = preprocess(frame)
+    last_frame = frame
     action = eval_network(net, frame)
     done = False
     while not done:
         env.render()
         frame, reward, done, _ = env.step(action)
         total_reward += reward
+        frame = preprocess(frame)
+        # If the road disappears of the map or the car stops, exit the game with reward -= 80
+        if not frame.any() or np.array_equal(last_frame, frame):
+            done = True
+            total_reward -= 80
         action = eval_network(net, frame)
+#         print(action)
+        last_frame = frame
 
     return total_reward
 
@@ -63,10 +77,14 @@ def run(config_file):
     
     # Create the population, which is the top-level object for a NEAT run.
     p = neat.Population(config)
-#     p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-0')
+    # Or load one from a checkpointer
+#     p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-29')
 
     # Add a stdout reporter to show progress in the terminal.
     custom_stats = custom_report.StdOutReporter()
+    # In case you want to load it from a checkpointer add:
+#     custom_stats.load('neat-stats-29')
+
     p.add_reporter(custom_stats)
 #     stats = neat.StatisticsReporter()
 #     p.add_reporter(stats)

3d638056f28c6caaa614a5616419e06d5f569b5a
Joan <jlloppalao@gmail.com>
2020-01-05 16:53:33 +0100

    fixed the puntuetion reward when finishing

--- game_improved.py
+++ game_improved.py
@@ -14,9 +14,10 @@ import custom_report
 env = gym.make('CarRacing-v0')
 
 NUM_GENERATIONS = 1000
+PATIENCE = 0
 
 def map_image(pixel):
-    if pixel[1] < 200:
+    if pixel[1] < 120:
         return 1
     return 0
 
@@ -28,14 +29,14 @@ def preprocess(frame):
     # Simplify the map by setting the road to 1 and everything else to 0
     frame = np.array([[map_image(pixel) for pixel in row] for row in frame])
     frame = frame[:83, :]
-    frame = block_reduce(frame, (5, 5), np.mean)
+    frame = block_reduce(frame, (10, 10), np.mean)
     frame = frame.flatten()
     return frame
 
 
 def eval_network(net, frame):
 #     print(frame.shape)
-    assert (frame.shape == (340,))
+    assert (frame.shape == (90,))
     result = net.activate(frame)
     assert (len(result) == 3)
     return result
@@ -48,24 +49,37 @@ def eval_genome(genome, config):
 
     """
     net = neat.nn.FeedForwardNetwork.create(genome, config)
-    total_reward = 0.0
+    total_reward = 6.0
     frame = env.reset()
     frame = preprocess(frame)
     last_frame = frame
     action = eval_network(net, frame)
     done = False
+    steps = 0
+    patience_count = 0
     while not done:
         env.render()
         frame, reward, done, _ = env.step(action)
         total_reward += reward
         frame = preprocess(frame)
-        # If the road disappears of the map or the car stops, exit the game with reward -= 80
-        if not frame.any() or np.array_equal(last_frame, frame):
+        # If the road disappears of the map, exit the game with reward -= 80
+        if not frame.any():
             done = True
             total_reward -= 80
+            print('finished with total reward of:', total_reward)
+        # If the car stops exit the game with the reward that we would have if we have wait to the end.
+        if np.array_equal(last_frame, frame):
+            if patience_count == PATIENCE:
+                done = True
+                total_reward += -0.1 * (1000 - steps)
+                print('finished with total reward of:', total_reward)
+            else:
+                patience_count += 1
+        else:
+            patience_count = 0
         action = eval_network(net, frame)
-#         print(action)
         last_frame = frame
+        steps += 1
 
     return total_reward
 
@@ -78,12 +92,12 @@ def run(config_file):
     # Create the population, which is the top-level object for a NEAT run.
     p = neat.Population(config)
     # Or load one from a checkpointer
-#     p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-29')
+#     p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-31')
 
     # Add a stdout reporter to show progress in the terminal.
     custom_stats = custom_report.StdOutReporter()
     # In case you want to load it from a checkpointer add:
-#     custom_stats.load('neat-stats-29')
+#     custom_stats.load('neat-stats-31')
 
     p.add_reporter(custom_stats)
 #     stats = neat.StatisticsReporter()
